fa630422782320fe90eb251f3a423d9fa857679c
lasbe <devlasbe@gmail.com>
2026-01-20 22:17:23 +0900

    chore: 환경 명칭 수정

--- apps/web/lib/fetchService.ts
+++ apps/web/lib/fetchService.ts
@@ -1,7 +1,7 @@
 const apiDev = process.env.NEXT_PUBLIC_API_URL_DEV;
-const apiOp = process.env.NEXT_PUBLIC_API_URL_OP;
+const apiProd = process.env.NEXT_PUBLIC_API_URL_PROD;
 const isDev = process.env.NODE_ENV === 'development';
-const defaultUrl = isDev ? apiDev : apiOp;
+const defaultUrl = isDev ? apiDev : apiProd;
 
 type MyFetchType = {
   path: RequestInfo | URL;
--- apps/web/services/defaultUrl.ts
+++ apps/web/services/defaultUrl.ts
@@ -1,5 +1,5 @@
 const apiDev = process.env.NEXT_PUBLIC_API_URL_DEV;
-const apiOp = process.env.NEXT_PUBLIC_API_URL_OP;
+const apiProd = process.env.NEXT_PUBLIC_API_URL_PROD;
 const isDev = process.env.NODE_ENV === "development";
-const defaultUrl = isDev ? apiDev : apiOp;
+const defaultUrl = isDev ? apiDev : apiProd;
 export default defaultUrl;
